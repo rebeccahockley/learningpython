72219e5a0277a57696d486adc7e6d015e840ec13
rebeccahockley <hockley.r@yahoo.com>
2022-10-06 15:45:20 +0200

    First commands learnt

---
+++ app.py
@@ -0,0 +1,17 @@
+print("Rebecca")
+#name = "John Smith"
+#age = 20
+#new_patient = True
+
+#name = input("What is your name? ")
+#print("Hi " + name)
+#colour = input("What is your favourite colour? ")
+#print(name + " likes the colour " + colour)
+#fav_object = input("Name an object that is that colour ")
+#print(fav_object + " is " + colour)
+
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
